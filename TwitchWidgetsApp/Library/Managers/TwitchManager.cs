e697f34c3eac1940abcdfad3729d5c746382a154
Mario Steele <mario@ruby-im.net>
2024-03-25 22:06:40 -0500

    Ensured proper Nullable References for all fields.
    Fixed ChatOnMessageReceived() to ensure that messages are always added to the Global History.

--- TwitchWidgetsApp/Library/Managers/TwitchManager.cs
+++ TwitchWidgetsApp/Library/Managers/TwitchManager.cs
@@ -22,20 +22,20 @@ public partial class TwitchManager : Node
     [Signal] public delegate void NewConnectionEventHandler();
     [Signal] public delegate void EventSubReconnectEventHandler();
     
-    [Singleton] public Globals Globals;
-    public TwitchConnection Connection;
-    private TwitchConnection _botConnection;
-    public UserModel Streamer;
-    public UserModel Bot;
-    public EventSub.EventSub EventSub;
-    public ChatClient Chat;
+    [Singleton] public Globals? Globals;
+    public TwitchConnection? Connection;
+    private TwitchConnection? _botConnection;
+    public UserModel? Streamer;
+    public UserModel? Bot;
+    public EventSub.EventSub? EventSub;
+    public ChatClient? Chat;
     public bool IsReady = false;
-    public bool IsChatConnected => Chat.IsOpen();
-    public bool IsEventSubConnected => EventSub.IsOpen();
+    public bool IsChatConnected => Chat!.IsOpen();
+    public bool IsEventSubConnected => EventSub!.IsOpen();
 
     private bool _isCurrentlyMock = false;
 
-    private RandomNumberGenerator _rng;
+    private RandomNumberGenerator? _rng;
     
     private List<Color> _twitchColors =
     [
@@ -61,37 +61,37 @@ public partial class TwitchManager : Node
         this.OnReady();
         _rng = new RandomNumberGenerator();
         _rng.Randomize();
-        Globals.CredentialsUpdated += LoadConnectionInfo;
-        Globals.SettingsLoaded += GlobalsOnSettingsLoaded;
-        Globals.SettingsUpdated += GlobalsOnSettingsUpdated;
+        Globals!.CredentialsUpdated += LoadConnectionInfo;
+        Globals!.SettingsLoaded += GlobalsOnSettingsLoaded;
+        Globals!.SettingsUpdated += GlobalsOnSettingsUpdated;
     }
 
     private void GlobalsOnSettingsUpdated()
     {
-        if (Globals.UseMockServer == _isCurrentlyMock) return;
+        if (Globals!.UseMockServer == _isCurrentlyMock) return;
         
         var currentEventSub = EventSub;
-        EventSub = new EventSub.EventSub(Connection, Globals.UseMockServer);
+        EventSub = new EventSub.EventSub(Connection!, Globals.UseMockServer);
         _isCurrentlyMock = Globals.UseMockServer;
         
-        if (!currentEventSub.IsOpen()) return;
+        if (!currentEventSub!.IsOpen()) return;
         
         currentEventSub.Disconnect();
         EventSub.Connect();
         EventSub.OnConnected += async (sender, args) =>
         {
-            await EventSub.Subscribe([
+            await EventSub!.Subscribe([
                 SubscribeEnum.ChannelRaid, SubscribeEnum.ChannelSubscription, SubscribeEnum.ChannelFollow,
                 SubscribeEnum.ChannelCheer, SubscribeEnum.ChannelSubscriptionGifted,
                 SubscribeEnum.ChannelSubscriptionMessage
-            ], Streamer.id);
+            ], Streamer!.id);
         };
     }
 
     private async void GlobalsOnSettingsLoaded()
     {
         LoadConnectionInfo();
-        if (Globals.AutoConnectTwitch)
+        if (Globals!.AutoConnectTwitch)
         {
             while (!IsReady)
             {
@@ -103,7 +103,7 @@ public partial class TwitchManager : Node
 
     private async void LoadConnectionInfo()
     {
-        if (Globals.Database?.Secrets == null) { Globals.RunOnMain(LoadConnectionInfo); return; }
+        if (Globals!.Database?.Secrets == null) { Globals.RunOnMain(LoadConnectionInfo); return; }
 
         var res = Globals.Database.Secrets.ToList();
         if (res.Count == 0) return;
@@ -132,7 +132,7 @@ public partial class TwitchManager : Node
         
         Chat = new ChatClient(_botConnection);
         Chat.OnMessageReceived += ChatOnMessageReceived;
-        EventSub = new EventSub.EventSub(Connection, Globals.UseMockServer);
+        EventSub = new EventSub.EventSub(Connection!, Globals!.UseMockServer);
         EventSub.OnReconnectRequested += EventSubOnReconnectRequested;
         _isCurrentlyMock = Globals.UseMockServer;
         EmitSignal(SignalName.NewConnection);
@@ -140,38 +140,38 @@ public partial class TwitchManager : Node
         EmitSignal(SignalName.ManagerReady);
     }
 
-    private async void ChatOnMessageReceived(object sender, ChatMessagePacketModel e)
+    private async void ChatOnMessageReceived(object? sender, ChatMessagePacketModel e)
     { 
-        var user = Globals.Chatters.FirstOrDefault(x => x.id == e.UserID);
+        var user = Globals!.Chatters.FirstOrDefault(x => x.id == e.UserID);
         if (user != null)
         {
             var kkc = Globals.KnownChatters.FirstOrDefault(x => x.TwitchId == e.UserID);
             if (kkc == null)
             {
                 GD.PrintErr("Chatter not saved in Database: " + e.UserID);
-                return;
+            } else {
+                kkc.LastSeen = DateTime.UtcNow;
+                await Globals!.Database!.SaveChangesAsync();
             }
-            kkc.LastSeen = DateTime.UtcNow;
-            await Globals.Database.SaveChangesAsync();
-            return;
+        } else {
+            user = await Globals.TwitchApi.Users.GetUserByID(e.UserID);
+
+            if (user == null) return;
         }
         
-        user = await Globals.TwitchApi.Users.GetUserByID(e.UserID);
-
-        if (user == null) return;
         
         Globals.Chatters.Add(user);
         var color = Color.FromHtml(e.Color);
         if (color == Colors.Black)
         {
-            var indx = (int)_rng.Randi() % _twitchColors.Count;
+            var indx = (int)_rng!.Randi() % _twitchColors.Count;
             if (indx < 0) indx *= -1;
             color = _twitchColors[indx];
             Globals.SavedChatColors[user] = color;
         }
         Globals.SavedChatColors[user] = color;
 
-        var kc = Globals.Database.KnownChatters!.FirstOrDefault(x => x.TwitchId == user.id);
+        var kc = Globals!.Database!.KnownChatters!.FirstOrDefault(x => x.TwitchId == user.id);
         if (kc == null)
         {
             kc = new KnownChatter()
@@ -183,7 +183,7 @@ public partial class TwitchManager : Node
                 FirstSeen = DateTime.UtcNow,
                 LastSeen = DateTime.UtcNow
             };
-            Globals.Database.KnownChatters.Add(kc);
+            Globals!.Database!.KnownChatters!.Add(kc);
             await Globals.Database.SaveChangesAsync();
         }
         else
@@ -195,18 +195,18 @@ public partial class TwitchManager : Node
         
         Globals.ChatHistory.Add(e);
         
-        Globals.AlertManager.NewChatterAlert(user, e);
+        Globals!.AlertManager!.NewChatterAlert(user, e);
     }
 
-    private void EventSubOnReconnectRequested(object sender, string e)
+    private void EventSubOnReconnectRequested(object? sender, string e)
     {
         GD.Print($"Reconnect message came through, Reconnect URL: {e}");
         var oldEventSub = EventSub;
-        EventSub = new EventSub.EventSub(Connection, Globals.UseMockServer, true);
+        EventSub = new EventSub.EventSub(Connection!, Globals!.UseMockServer, true);
         _isCurrentlyMock = Globals.UseMockServer;
         EmitSignal(SignalName.EventSubReconnect);
         EventSub.Connect(e);
-        if (oldEventSub.IsOpen())
+        if (oldEventSub!.IsOpen())
             oldEventSub.Disconnect();
     }
 
@@ -228,7 +228,7 @@ public partial class TwitchManager : Node
         }
 
         if (!anyChanges) return;
-        Globals.Database.SaveChanges();
+        Globals!.Database!.SaveChanges();
     }
 
     public async void ConnectTwitch()
@@ -236,35 +236,35 @@ public partial class TwitchManager : Node
         if (!IsReady) return;
 
         // Chat Client Stuff
-        Chat.OnPingReceived += (sender, args) =>
+        Chat!.OnPingReceived += (sender, args) =>
         {
             GD.Print("Ping? PONG!");
             Chat.Pong();
         };
         Chat.OnGlobalUserStateReceived += (sender, model) => Chat.Join(Streamer);
-        Chat.OnUserListReceived += (sender, model) => Chat.SendMessage(Streamer, $"{Bot.display_name} is now online.");
+        Chat.OnUserListReceived += (sender, model) => Chat.SendMessage(Streamer, $"{Bot!.display_name} is now online.");
         await Chat.Connect();
 
-        EventSub.Connect();
+        EventSub!.Connect();
         EventSub.OnConnected += async (sender, args) =>
         {
             await EventSub.Subscribe([
                 SubscribeEnum.ChannelRaid, SubscribeEnum.ChannelSubscription, SubscribeEnum.ChannelFollow,
                 SubscribeEnum.ChannelCheer, SubscribeEnum.ChannelSubscriptionGifted,
                 SubscribeEnum.ChannelSubscriptionMessage
-            ], Streamer.id);
+            ], Streamer!.id);
         };
     }
 
     public void DisconnectTwitch()
     {
         // Chat Client Stuff
-        Chat.Disconnect();
+        Chat!.Disconnect();
         Chat = new ChatClient(_botConnection);
         
         // Event Sub Stuff
-        EventSub.Disconnect();
-        EventSub = new EventSub.EventSub(Connection, Globals.UseMockServer);
+        EventSub!.Disconnect();
+        EventSub = new EventSub.EventSub(Connection!, Globals!.UseMockServer);
         _isCurrentlyMock = Globals.UseMockServer;
         EventSub.OnReconnectRequested += EventSubOnReconnectRequested;
         
