90c6ca6354cd6aec2449e72dacd6b5dc101aec13
Mario Steele <mario@ruby-im.net>
2025-02-09 03:39:59 -0600

    Updated SettingsFile Resource
    Added Setters for all variables to ensure we are emitting when we have changed.
    Organized settings into regions for better readability.
    Added bunch of Setttings.
    Added snapshot() and restore(), to take a snapshot of the current settings, so if the user modifies values, but cancels the dialog, we restore the settings back to their original values.

--- Resources/settings.gd
+++ Resources/settings.gd
@@ -1,13 +1,99 @@
 class_name SettingsFile extends Resource
 
-@export var client_id : String
-@export var client_secret : String
+#region Twitch Connection
+@export var client_id : String :
+	set(value):
+		if client_id != value:
+			client_id = value
+			emit_changed()
+@export var client_secret : String :
+	set(value):
+		if client_secret != value:
+			client_secret = value
+			emit_changed()
+#endregion
 
-@export var music_folders : Array[String]
-@export var randomize_music : bool = false
-@export var loop_playlist : bool = false
-@export var music_vol : float = 1.0
-@export var autoplay_music : bool = false
+#region Music Settings
+@export var music_folders : Array[String] :
+	set(value):
+		if music_folders != value:
+			music_folders = value
+			emit_changed()
+@export var randomize_music : bool :
+	set(value):
+		if randomize_music != value:
+			randomize_music = value
+			emit_changed()
+@export var loop_playlist : bool :
+	set(value):
+		if loop_playlist != value:
+			loop_playlist = value
+			emit_changed()
+@export var music_vol : float :
+	set(value):
+		if music_vol != value:
+			music_vol = value
+			emit_changed()
+@export var autoplay_music : bool :
+	set(value):
+		if autoplay_music != value:
+			autoplay_music = value
+			emit_changed()
+#endregion
+
+#region Scene Collection & Alert Set
+@export var collection_name : String :
+	set(value):
+		if collection_name != value:
+			collection_name = value
+			emit_changed()	
+@export var selected_alert_set : String :
+	set(value):
+		if selected_alert_set != value:
+			selected_alert_set = value
+			emit_changed()
+#endregion
+
+#region Startup
+@export var auto_connect_twitch : bool:
+	set(value):
+		if auto_connect_twitch != value:
+			auto_connect_twitch = value
+			emit_changed()
+@export var auto_connect_obs : bool:
+	set(value):
+		if auto_connect_obs != value:
+			auto_connect_obs = value
+			emit_changed()
+@export var start_fullscreen : bool:
+	set(value):
+		if start_fullscreen != value:
+			start_fullscreen = value
+			emit_changed()
+@export var use_screen : bool:
+	set(value):
+		if use_screen != value:
+			use_screen = value
+			emit_changed()
+@export var selected_screen : int:
+	set(value):
+		if selected_screen != value:
+			selected_screen = value
+			emit_changed()
+@export var log_debug : bool:
+	set(value):
+		if log_debug != value:
+			log_debug = value
+			emit_changed()
+@export var log_to_console : bool:
+	set(value):
+		if log_to_console != value:
+			log_to_console = value
+			emit_changed()
+#endregion
+
+func _init() -> void:
+	music_vol = 1.0
 
 func save_settings() -> void:
 	ResourceSaver.save(self, "user://settings.res")
@@ -16,3 +102,15 @@ static func load_settings() -> SettingsFile:
 	if FileAccess.file_exists("user://settings.res"):
 		return ResourceLoader.load("user://settings.res") as SettingsFile
 	return new()
+
+var _stage : SettingsFile
+
+func snapshot() -> void:
+	_stage = duplicate(true)
+
+func restore() -> void:
+	for prop in get_property_list():
+		if prop.usage & PROPERTY_USAGE_SCRIPT_VARIABLE == PROPERTY_USAGE_SCRIPT_VARIABLE:
+			set(prop.name, _stage.get(prop.name))
+	_stage = null
+	emit_changed()
