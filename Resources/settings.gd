c706b3a1c0357d645ecde5f215fb9f9e402f1495
Mario Steele <mario@ruby-im.net>
2025-02-08 22:28:17 -0600

    Updated Settings Resource File

--- Resources/settings.gd
+++ Resources/settings.gd
@@ -3,6 +3,12 @@ class_name SettingsFile extends Resource
 @export var client_id : String
 @export var client_secret : String
 
+@export var music_folders : Array[String]
+@export var randomize_music : bool = false
+@export var loop_playlist : bool = false
+@export var music_vol : float = 1.0
+@export var autoplay_music : bool = false
+
 func save_settings() -> void:
 	ResourceSaver.save(self, "user://settings.res")
 
