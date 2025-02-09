65a060a34af664940f8a0d7006be8803d43d8869
Mario Steele <mario@ruby-im.net>
2025-02-08 22:28:28 -0600

    Updated Settings Manager

--- Library/autoloads/settings.gd
+++ Library/autoloads/settings.gd
@@ -2,5 +2,14 @@ class_name SettingsManager extends Node
 
 var data : SettingsFile
 
+signal loaded()
+signal updated()
+
 func _enter_tree() -> void:
 	data = SettingsFile.load_settings()
+	loaded.emit()
+
+func save_settings(emitsignal : bool = true) -> void:
+	data.save_settings()
+	if emitsignal:
+		updated.emit()
