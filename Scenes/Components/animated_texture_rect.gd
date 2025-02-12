110df72c7162c51a444a50db540468f9c030fddc
Mario Steele <mario@ruby-im.net>
2025-02-12 11:51:59 -0600

    Created AnimatedTextureRect
    Created a TextureRect that takes SpriteFrames.  When given sprite
    frames, will process it as though it was an animation to play, and will
    play based upon the given frame information.

---
+++ Scenes/Components/animated_texture_rect.gd
@@ -0,0 +1,37 @@
+@tool
+class_name AnimatedTextureRect extends TextureRect
+
+var _currentFrame : int = -1
+
+@export var frames : SpriteFrames :
+	get():
+		return frames
+	set(value):
+		frames = value
+		if frames == null:
+			return
+		if is_node_ready():
+			var frames = frames.get_frame_count(&"default")
+			if frames > 1:
+				play_animation()
+			else:
+				show_static()
+
+func play_animation() -> void:
+	if frames == null:
+		_currentFrame = -1
+		return
+	_currentFrame += 1
+	if _currentFrame > frames.get_frame_count(&"default"):
+		if not frames.get_animation_loop(&"default"):
+			return
+		_currentFrame = 0
+	var duration = frames.get_frame_duration(&"default", _currentFrame)
+	_show_frame(_currentFrame)
+	get_tree().create_timer(duration).timeout.connect(play_animation)
+
+func show_static() -> void:
+	_show_frame(0)
+
+func _show_frame(frame : int) -> void:
+	texture = frames.get_frame_texture(&"default", frame)
