8d9c91f42fc403b2846b6958720ed255f90519bf
Mario Steele <mario@ruby-im.net>
2025-02-12 11:48:05 -0600

    Updated Utils
    Added lazy_* functions for giving the lazy counting, lazy time, and
    random_lazy_count().
    Added first() function, to follow similar pattern as filter(), map(),
    etc etc.

--- Library/utils.gd
+++ Library/utils.gd
@@ -1,3 +1,4 @@
+@tool
 class_name Utils extends RefCounted
 
 static func format_time(secs : float) -> String:
@@ -9,3 +10,31 @@ static func format_time(secs : float) -> String:
 		return "%02d:%02d:%02d" % [hours, minutes, secs]
 	else:
 		return "%02d:%02d" % [minutes, secs]
+
+static func lazy_count(i : int) -> String:
+	if i > 1_000_000:
+		return "%0.2dm" % i
+	if i > 1_000:
+		return "%0.2dk" % i
+	if i > 0:
+		return "%d" % i
+	return ""
+
+static func random_lazy_count() -> String:
+	var i = randi_range(0,5_000_000)
+	return lazy_count(i)
+
+static func lazy_time(secs : float) -> String:
+	if secs < 60:
+		return "%ds" % secs
+	elif secs < 3600:
+		return "%dm" % int(secs / 60)
+	elif secs < 86400:
+		return "%dh" % int(secs / 3600)
+	else:
+		return "%dd" % int(secs / 86400)
+
+static func first(array : Array, lambda : Callable) -> Variant:
+	var find = array.filter(lambda)
+	if find.size() > 0: return find[0]
+	return null
