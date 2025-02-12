a352ffc8814259b908e11a7fea2d48d97f055fdf
Mario Steele <mario@ruby-im.net>
2025-02-12 12:05:21 -0600

    Updated MainScreen Scene
    Added to ready function, setting the main_window for the SceneManager,
    so that functions can be properly called.
    Changes the way transitions occur, instead of one fade out, then one
    fade in, we fade out and fade in in parallel.

--- Scenes/main_screen.gd
+++ Scenes/main_screen.gd
@@ -1,4 +1,4 @@
-extends Control
+class_name MainScreen extends Control
 
 const NO_SCENE = preload("res://Scenes/NoSceneSelected.tscn")
 var _current_scene : Control
@@ -10,13 +10,14 @@ func _ready() -> void:
 	_current_scene = scene
 	Managers.streamdeck.settings.connect(_handle_settings)
 	Managers.streamdeck.shutdown.connect(_handle_shutdown)
+	Managers.scene.main_window = self
 
 func switch_scene(packed : PackedScene) -> void:
 	_next_scene = packed.instantiate()
 	add_child(_next_scene)
-	move_child(_next_scene,0)
 	_next_scene.modulate.a = 0
-	var tween = create_tween()
+	move_child(_next_scene,0)
+	var tween = create_tween().set_parallel()
 	tween.tween_property(_current_scene, "modulate:a", 0.0, 1.0)
 	tween.tween_property(_next_scene, "modulate:a", 1.0, 1.0)
 	await tween.finished
