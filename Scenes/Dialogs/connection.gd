012ef60c7deec041c0e61fb779d67ce16f6f589d
Mario Steele <mario@ruby-im.net>
2025-02-13 03:30:26 -0600

    Updated Connection tab in settings dialog
    Added checks to see when initializing if we are connected to OBS, if so,
    we will get the current status of Streaming and Recording.
    Added Event handlers to handle events for recording state changes, and
    streaming state changes.

--- Scenes/Dialogs/connection.gd
+++ Scenes/Dialogs/connection.gd
@@ -7,6 +7,18 @@ func _ready() -> void:
 	%ObsPort.text = str(Managers.settings.data.obs_port)
 	%ObsPassword.text = Managers.settings.data.obs_pass
 	
+	if Managers.obs.is_connected_to_host():
+		%ConnectStatus.text = "[color=green]Connected[/color]"
+		var streaming := await Managers.obs.get_stream_status()
+		var recording := await Managers.obs.get_record_status()
+		if streaming.output_active:
+			%StreamingStatus.text = "[color=green]Streaming[/color]"
+		if recording.output_active:
+			%RecordingStatus.text = "[color=green]Recording[/color]"
+	
+	Managers.obs.streaming_state.connect(_handle_stream_state)
+	Managers.obs.recording_state.connect(_handle_record_state)
+	
 	%Authorize.pressed.connect(func():
 		var id = %ClientId.text
 		var secret = %ClientSecret.text
@@ -48,3 +60,15 @@ func _handle_fail_obs(code : int = -1, message : String = "Unable to connect to 
 	Managers.obs.connection_closed_clean.disconnect(_handle_fail_obs)
 	%ConnectOBS.disabled = false
 	Managers.obs.reset_obs()
+
+func _handle_stream_state(streaming : bool) -> void:
+	if streaming:
+		%StreamingStatus.text = "[color=green]Streaming[/color]"
+	else:
+		%StreamingStatus.text = "[color=red]Offline[/color]"
+
+func _handle_record_state(recording : bool, _path : String) -> void:
+	if recording:
+		%RecordingStatus.text = "[color=green]Recording[/color]"
+	else:
+		%RecordingStatus.text = "[color=red]Not Recording[/color]"
