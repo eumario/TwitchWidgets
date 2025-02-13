5d708995390107c7e62f64ec46cb9f9d412fe572
Mario Steele <mario@ruby-im.net>
2025-02-13 03:24:48 -0600

    Updated ObsManager
    Added signals for scene_changed, streaming_state, and recording_state.
    Added functions to get_current_program_scene(), get_stream_status() and
    get_recording_status()
    Added to events for StreamStateChanged and RecordingStateChanged
    Added classes for responses from get_stream_status() and
    get_recording_status().

--- Library/autoloads/obs.gd
+++ Library/autoloads/obs.gd
@@ -7,6 +7,9 @@ signal connection_ready()
 signal connection_failed()
 signal connection_closed_clean()
 signal error(message : String)
+signal scene_changed(scene_name : String)
+signal streaming_state(live : bool)
+signal recording_state(recording : bool, path : String)
 #endregion
 
 #region OBSManager Signals
@@ -50,13 +53,73 @@ func reset_obs() -> void:
 	remove_child(connection)
 	connection.queue_free()
 	_ready()
+
+func get_current_program_scene() -> String:
+	var res := connection.make_generic_request("GetCurrentProgramScene", {})
+	await res.response_received
+	Logger.debug("ObsManager: GetCurrentProgramScene() -> %s" % res.message.response_data.scene_name)
+	return res.message.response_data.scene_name
+
+func get_stream_status() -> StreamStatus:
+	var res := connection.make_generic_request("GetStreamStatus", {})
+	await res.response_received
+	Logger.debug("ObsManager: GetStreamStatus()")
+	#var data := NoOBSWS.Message.camel_to_snake_recursive(res.message.response_data)
+	return StreamStatus.new(res.message.response_data)
+
+func get_record_status() -> RecordStatus:
+	var res := connection.make_generic_request("GetRecordStatus", {})
+	await res.response_received
+	Logger.debug("ObsManager: GetRecordStatus()")
+	#var data := NoOBSWS.Message.camel_to_snake_recursive(res.message.response_data)
+	return RecordStatus.new(res.message.response_data)
 #endregion
 
 #region Signal Handlers
 func _handle_event(event : NoOBSWS.Message) -> void:
 	var data = event.get_data()
-	print(data)
+	
 	match data.event_type:
 		"CurrentProgramSceneChanged":
-			Managers.scene.scene_changed(data.event_data.scene_name)
+			scene_changed.emit(data.event_data.scene_name)
+			Logger.debug("ObsManager: CurrentProgramSceneChanged('%s')" % data.event_data.scene_name)
+		"StreamStateChanged":
+			if data.event_data.output_state == "OBS_WEBSOCKET_OUTPUT_STARTED" or \
+				data.event_data.output_state == "OBS_WEBSOCKET_OUTPUT_STOPPED":
+				streaming_state.emit(data.event_data.output_active)
+				Logger.debug("ObsManager: StreamStateChanged(%s)" % data.event_data.output_active)
+		"RecordStateChanged":
+			if data.event_data.output_state == "OBS_WEBSOCKET_OUTPUT_STARTED" or \
+				data.event_data.output_state == "OBS_WEBSOCKET_OUTPUT_STOPPED":
+				recording_state.emit(data.event_data.output_active, data.event_data.output_path)
+				Logger.debug("ObsManager: RecordStateChanged(%s, %s)" % [data.event_data.output_active, data.event_data.output_path])
+#endregion
+
+#region Support Classes
+class StreamStatus extends RefCounted:
+	var output_active : bool
+	var output_reconnecting : bool
+	var output_timecode : String
+	var output_duration : int
+	var output_congestion : int
+	var output_bytes : int
+	var output_skipped_frames : int
+	var output_total_frames : int
+	
+	func _init(d : Dictionary = {}) -> void:
+		for i in d:
+			if i in self:
+				self[i] = d[i]
+
+class RecordStatus extends RefCounted:
+	var output_active : bool
+	var output_paused : bool
+	var output_timecode : String
+	var output_duration : int
+	var output_bytes : int
+	
+	func _init(d : Dictionary = {}) -> void:
+		for i in d:
+			if i in self:
+				self[i] = d[i]
 #endregion
