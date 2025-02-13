e7e41e1f8b6047d161faf25221a3085409f3d746
Mario Steele <mario@ruby-im.net>
2025-02-13 03:27:22 -0600

    Updated SceneManager.gd
    Added signals for connection_ready and scene_changed.
    Added current_scene to show what our current scene should be, per Obs.
    Added when connected to Obs, we get our current scene, and automatically
    switch to said scene.
    Updated namign to scene_collection_name, to prevent shadowing.

--- Library/autoloads/scene.gd
+++ Library/autoloads/scene.gd
@@ -7,6 +7,7 @@ var _scenes : Array[SceneCollection] = []
 #region Properties
 var current_collection : SceneCollection = null
 var main_window : MainScreen = null
+var current_scene_name : String = ""
 #endregion
 
 #region Godot Overrides
@@ -22,6 +23,9 @@ func _ready() -> void:
 			set_current_collection(Managers.settings.data.collection_name)
 			Logger.info("INIT: Scene Collection set to %s" % current_collection.collection_name)
 	)
+	
+	Managers.obs.connection_ready.connect(_handle_connect)
+	Managers.obs.scene_changed.connect(_handle_scene_changed)
 #endregion
 
 #region Public API
@@ -31,16 +35,29 @@ func get_scene_collections() -> Array:
 func add_scene_collection(coll : SceneCollection) -> void:
 	_scenes.append(coll)
 
-func set_current_collection(name : String) -> void:
-	current_collection = Utils.first(_scenes, func(x : SceneCollection): return x.collection_name == name)
+func set_current_collection(scene_collection_name : String) -> void:
+	current_collection = Utils.first(_scenes, func(x : SceneCollection): return x.collection_name == scene_collection_name)
 
-func scene_changed(name : String) -> void:
+func scene_changed(scene_collection_name : String) -> void:
 	if current_collection == null: return
 	if main_window == null: return
 	
-	if current_collection.scenes.has(name):
-		Logger.info("Switching to scene '%s'" % name)
-		main_window.switch_scene(current_collection.scenes[name])
+	if current_collection.scenes.has(scene_collection_name):
+		Logger.info("Switching to scene '%s'" % scene_collection_name)
+		main_window.switch_scene(current_collection.scenes[scene_collection_name])
+		current_scene_name = scene_collection_name
 	else:
 		Logger.error("No such Scene in scene collection '%s'" % current_collection.collection_name)
+
+
+#endregion
+
+#region Signal Handlers
+func _handle_connect() -> void:
+	var res = await Managers.obs.get_current_program_scene()
+	if current_scene_name != res:
+		scene_changed(res)
+
+func _handle_scene_changed(scene_name : String) -> void:
+	scene_changed(scene_name)
 #endregion
