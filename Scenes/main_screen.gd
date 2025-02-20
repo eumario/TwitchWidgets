145ba0e9d1d6410c2d6f5a2d4cca4398d32e89b1
Mario Steele <mario@ruby-im.net>
2025-02-20 04:31:03 -0600

    Updated Main Screen.
    Added switching bool, to prevent fast switching from occuring, and causing _current_scene and _next_scene from getting out of sync.
    Added settings check for Use Screen, and which screen to appear on, as well as FullScreen.

--- Scenes/main_screen.gd
+++ Scenes/main_screen.gd
@@ -3,6 +3,7 @@ class_name MainScreen extends Control
 const NO_SCENE = preload("res://Scenes/NoSceneSelected.tscn")
 var _current_scene : Control
 var _next_scene : Control
+var _switching : bool = false
 
 func _ready() -> void:
 	var scene = NO_SCENE.instantiate()
@@ -11,16 +12,25 @@ func _ready() -> void:
 	Managers.streamdeck.settings.connect(_handle_settings)
 	Managers.streamdeck.shutdown.connect(_handle_shutdown)
 	Managers.scene.main_window = self
+	if Managers.settings.data.use_screen:
+		get_window().current_screen = Managers.settings.data.selected_screen
+	
+	if Managers.settings.data.start_fullscreen:
+		get_window().mode = Window.MODE_FULLSCREEN
 
 func switch_scene(packed : PackedScene) -> void:
+	while _switching:
+		await get_tree().process_frame
 	_next_scene = packed.instantiate()
 	add_child(_next_scene)
 	_next_scene.modulate.a = 0
 	move_child(_next_scene,0)
 	var tween = create_tween().set_parallel()
-	tween.tween_property(_current_scene, "modulate:a", 0.0, 1.0)
-	tween.tween_property(_next_scene, "modulate:a", 1.0, 1.0)
+	tween.tween_property(_current_scene, "modulate:a", 0.0, 0.5)
+	tween.tween_property(_next_scene, "modulate:a", 1.0, 0.5)
+	_switching = true
 	await tween.finished
+	_switching = false
 	_current_scene.queue_free()
 	_current_scene = _next_scene
 	_next_scene = null
